87f154da1fad3722e6b31feee3c1f425fdfb7325
CelesteC219 <eule88219@gmail.com>
2020-11-14 12:06:27 +1000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.getElementById("nav").innerHTML = `<div class="ham">☰</div>
+<ul class="nav-content">
+    <li><a href="/index.html"accesskey="A">About Us</a></li>
+    <li><a href="/html/content.html"accesskey="P">Products</a></li>
+    <li><a href="/html/form.html"accesskey="F">Form</a></li>
+    <li><a href="/html/tableofdata.html"accesskey="D">Data</a></li>
+    <li><a href="/html/blog.html"accesskey="B">Blog</a></li>
+</ul>`;
+
+document.getElementById(
+  "footer"
+).innerHTML = `<p>&copy2020 contact number:04112345678</p>
+<ul>
+    <li><a href="/index.html">About Us</a></li>
+    <li><a href="/html/content.html">Products</a></li>
+    <li><a href="/html/form.html">Form</a></li>
+    <li><a href="/html/tableofdata.html">Data</a></li>
+    <li><a href="/html/blog.html">Blog</a></li>
+</ul>`;
